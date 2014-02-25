2aff3fa04a732fed61a21f440f3557d1b2b59d93
w1mvy <w1mvypine@gmail.com>
2014-02-25 16:09:59 +0900

    Add default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,3 +5,23 @@ default[:td_agent][:plugins] = []
 default[:td_agent][:includes] = false
 default[:td_agent][:default_config] = true
 
+# default distribute forwarder config file
+default[:td_agent][:server_type] = "forwarder"
+
+# for forwarder attributes
+default[:td_agent][:nginx_access_log_path]   = "/var/log/nginx/nginx_access.log"
+default[:td_agent][:nginx_error_log_path]    = "/var/log/nginx/nginx_error.log"
+default[:td_agent][:application_log_path]    = "/var/log/unicorn/unicorn.log"
+default[:td_agent][:aggregator_master_host]  = "127.0.0.1"
+default[:td_agent][:aggregator_standby_host] = "127.0.0.1"
+
+# for aggregator attributes
+default[:td_agent][:aggregator_port] = 24224
+default[:td_agent][:aws_key_id]      = ""
+default[:td_agent][:aws_sec_key]     = ""
+default[:td_agent][:s3_endpoint]     = ""
+default[:td_agent][:s3_bucket]       = ""
+
+default[:td_agent][:nginx_aggregator_server_name]     = "hogehoge"
+default[:td_agent][:nginx_aggregator_host] = "127.0.0.1"
+default[:td_agent][:nginx_aggregator_port] = 24224
